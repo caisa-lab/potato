921d4c49936b9fec83d1007d56aceed326e00694
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-05-27 15:53:50 +0200

    update data

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,16 @@ WORKDIR /app
 
 COPY ./potato /app/potato
 COPY ./requirements.txt /app/requirements.txt
+COPY ./task /app/task
+
 
 RUN pip install --upgrade pip && \
     pip install -r requirements.txt
 
+ENV PYTHONPATH=/app
+
 EXPOSE 8080
 
-CMD ["python", "potato/flask_server.py", "start", "project-hub/spot_yaml_generation/configs/task_config.yaml", "-p", "8080"]
+# Run the start script, it will check for an /app/prestart.sh script (e.g. for migrations)
+# And then will start Gunicorn with Uvicorn
+CMD ["/start.sh"]
---
+++ project-hub/spot-prompt-generation/preprocess_data.py
@@ -0,0 +1,27 @@
+import json
+from argparse import ArgumentParser
+from pathlib import Path
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument('--input-file', type=str, required=True)
+    parser.add_argument('--output-file', type=str, required=True)
+    parser.add_argument('--sample', type=int)
+
+    args = parser.parse_args()
+
+    input_file = Path(args.input_file)
+    output_file = args.output_file
+    sample = args.sample
+
+    with open(input_file) as f:
+        lines = f.readlines()
+        if sample < len(lines):
+            lines = lines[sample:]
+
+    with open(output_file, "w") as outfile:
+        for idx, line in enumerate(lines):
+            line = json.loads(line)
+            entry = {"id": f'{input_file.name}_{idx}', "text": line['prompt']}
+            print(json.dumps(entry), file=outfile)
